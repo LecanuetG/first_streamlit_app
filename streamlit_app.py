2f1ca6d8636e6afb49b12ac1a5e6c4c3652e4bba
LecanuetG <99472493+LecanuetG@users.noreply.github.com>
2022-11-23 19:53:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
-Import streamlit
+import streamlit
 
+streamlit.title('Bienvenue')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
