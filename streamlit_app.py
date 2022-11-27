6318282837ffbd610784045e889a84e6424325a1
LecanuetG <99472493+LecanuetG@users.noreply.github.com>
2022-11-27 20:08:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -26,7 +26,7 @@ streamlit.dataframe(fruits_to_show)
 
 #News section to display fruitvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response.json()) #just writes the data to the screen
+#streamlit.text(fruityvice_response.json()) #just writes the data to the screen
 
 # take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
